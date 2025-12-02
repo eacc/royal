640a2271846b9e16ab22d302394d06269c4713db
eacc <eacc61@gmail.com>
2025-12-01 20:48:37 -0500

    Second commit - Royal Car app

--- src/App.jsx
+++ src/App.jsx
@@ -40,13 +40,7 @@ import {
 // --- CONFIGURACIÓN DE FIREBASE (¡IMPORTANTE!) ---
 // Reemplaza esto con tu propia configuración de Firebase Console
 const firebaseConfig = {
-    apiKey: "AIzaSyBrTIqPjwLVVZNjSmsQ770kj-MDyvm7o-E",
-    authDomain: "royalcar-49e2e.firebaseapp.com",
-    projectId: "royalcar-49e2e",
-    storageBucket: "royalcar-49e2e.firebasestorage.app",
-    messagingSenderId: "931717650876",
-    appId: "1:931717650876:web:30afca96d09d94f960b3b9",
-    measurementId: "G-5F17KGGRJP"
+
 };
 
 // NOTA: Si estás probando sin configurar Firebase aún, la app dará error.
